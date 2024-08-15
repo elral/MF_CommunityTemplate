ec96c5e23ac92aaa9b57d1804422bc1c1801329c
elral <3263285+elral@users.noreply.github.com>
2024-08-15 13:04:02 +0200

    get copying community folder ot work

--- copy_fw_files.py
+++ copy_fw_files.py
@@ -9,13 +9,6 @@ if firmware_version == "":
 firmware_version = firmware_version.lstrip("v")
 firmware_version = firmware_version.strip(".")
 
-zip_file_name = 'Mobiflight-Connector'
-build_path = './_build'
-build_path_fw = build_path + '/firmware'
-build_path_json = build_path + '/Boards'
-distrubution_path = './_dist'
-board_folder = ['./_Boards/Atmel', './_Boards/RaspberryPi', './_Boards/ESP32']
-platform = env.BoardConfig().get("platform", {})
 # Get the ZIP filename from the build environment.
 community_project = env.GetProjectOption('custom_community_project', "")
 
@@ -25,52 +18,34 @@ custom_source_folder = env.GetProjectOption('custom_source_folder', "")
 # Get the foldername inside the zip file from the build environment.
 community_folder = env.GetProjectOption('custom_community_folder', "")
 
+platform = env.BoardConfig().get("platform", {})
 
 def copy_fw_files (source, target, env):
     fw_file_name=str(target[0])
 
-    if os.path.exists(build_path_fw) == False:
-        os.makedirs(build_path_fw)
-    if os.path.exists(build_path_json) == False:
-        os.makedirs(build_path_json)
-
-    if fw_file_name[-3:] == "bin" and platform == "raspberrypi":
+    if os.path.exists("./_build/" + custom_source_folder) == False:
+        os.makedirs("./_build/" + custom_source_folder + "/Community/firmware")
+        shutil.copytree(custom_source_folder + "/Community", "./_build/" + custom_source_folder + "/Community", dirs_exist_ok=True)
+        print("Creating /_build folder")
+    
+    if platform == "raspberrypi":
         fw_file_name=fw_file_name[0:-3] + "uf2"
 
     if platform == "espressif32":
         merge_bin(source, target, env)
         fw_file_name=fw_file_name[0:-4] + "_merged.bin"
 
-    # Copy build FW file
-    shutil.copy(fw_file_name, build_path_fw)
-    # merged bin file from ESP32 is missing!!
-
-    # Copy reset/uf2/json files
-    file_extension = '.hex'
-    copy_files_by_extension(board_folder, build_path_fw, file_extension)
-    file_extension = '.uf2'
-    copy_files_by_extension(board_folder, build_path_fw, file_extension)
-    file_extension = '.json'
-    copy_files_by_extension(board_folder, build_path_json, file_extension)
-
-    # Create ZIP file and add files from distrubution folder
-    zip_file_path = distrubution_path + '/' + zip_file_name + '_' + firmware_version + '.zip'
-    createZIP(build_path, zip_file_path, zip_file_name)
-
-
-def copy_files_by_extension(source_folders, target_folder, file_extension):
-    for source_folder in source_folders:
-        for root, dirs, files in os.walk(source_folder):
-            for file in files:
-                if file.endswith(file_extension):
-                    source_path = os.path.join(root, file)
-                    target_path = os.path.join(target_folder, file)
-                    shutil.copy2(source_path, target_path)
-
+    print("Copying community folder")
+    shutil.copy(fw_file_name, "./_build/" + custom_source_folder + "/Community/firmware")
+    original_folder_path = "./_build/" + custom_source_folder + "/Community"
+    zip_file_path = './_dist/' + community_project + '_' + firmware_version + '.zip'
+    new_folder_in_zip = community_folder
+    print("Creating zip file")
+    createZIP(original_folder_path, zip_file_path, new_folder_in_zip)
 
 def createZIP(original_folder_path, zip_file_path, new_folder_name):
-    if os.path.exists(distrubution_path) == False:
-        os.mkdir(distrubution_path)
+    if os.path.exists("./_dist") == False:
+        os.mkdir("./_dist")
     with zipfile.ZipFile(zip_file_path, 'w') as zipf:
         for root, dirs, files in os.walk(original_folder_path):
             for file in files:
